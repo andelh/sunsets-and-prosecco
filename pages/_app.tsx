73f83cb939304225fcf151877ad6b3b0791591f2
Andel Husbands <andelhusbands@gmail.com>
2023-03-15 14:45:50 -0400

    home page and project setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+
+const pally = localFont({
+  src: "./fonts/Pally-Variable.woff2",
+  variable: "--font-pally",
+});
+const dancingScript = localFont({
+  src: "./fonts/DancingScript-Variable.woff2",
+  variable: "--font-dancing",
+});
+export const panchang = localFont({
+  src: "./fonts/Panchang-Variable.woff2",
+  variable: "--font-panchang",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main
+      className={`${pally.variable} ${panchang.variable} ${dancingScript.variable} font-sans`}
+    >
+      <Component {...pageProps} />
+    </main>
+  );
 }
